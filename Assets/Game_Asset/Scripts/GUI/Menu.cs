ae6c4af51a3d1d1b11d7b825d66259e4ef3ae320
Emelyanov <anton575329@gmail.com>
2021-11-04 10:23:53 +0300

    Change & Fix GUI

--- Assets/Game_Asset/Scripts/GUI/Menu.cs
+++ Assets/Game_Asset/Scripts/GUI/Menu.cs
@@ -1,24 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
-{
-    [SerializeField] private GameObject _info;
-    [SerializeField] private GameObject _options;
-    public void OnRestartScene()
+namespace Game_Asset.Scripts.GUI
+{   
+    public class Menu : MonoBehaviour
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+        [SerializeField] private GameObject _info;
+        [SerializeField] private GameObject _options;
+        public void OnRestartScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
-    public void SetInfo()
-    {
-        _info.SetActive(!_info.activeSelf);
-        gameObject.SetActive(!gameObject.activeSelf);
-    }
+        public void SetInfo()
+        {
+            _info.SetActive(!_info.activeSelf);
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
 
-    public void SetOptions()
-    {
-        _options.SetActive(!_options.activeSelf);
-        gameObject.SetActive(!gameObject.activeSelf);
+        public void SetOptions()
+        {
+            _options.SetActive(!_options.activeSelf);
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
     }
 }
