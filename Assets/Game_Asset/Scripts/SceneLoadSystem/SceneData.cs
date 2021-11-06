a92f5827259ce680a2673cf8311dc3b4617ba63d
Ars-Mond <ars123.www@gmail.com>
2021-11-07 00:41:43 +0500

    SceneLoadSystem

---
+++ Assets/Game_Asset/Scripts/SceneLoadSystem/SceneData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor.SearchService;
+using UnityEngine;
+
+namespace Game_Asset.Scripts.SceneLoadSystem
+{
+    public class SceneData : MonoBehaviour
+    {
+        public SceneList scene;
+    }
+}
---
+++ Assets/Game_Asset/Scripts/SceneLoadSystem/SceneList.cs
@@ -0,0 +1,10 @@
+﻿namespace Game_Asset.Scripts.SceneLoadSystem
+{
+    public enum SceneList
+    {
+        Menu = 0,
+        Forest = 1,
+        Mountains = 2,
+        Temple = 3
+    }
+}
