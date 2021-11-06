8e8fcef15b2990057e297591c77667522530effb
Ars-Mond <ars123.www@gmail.com>
2021-11-06 19:58:30 +0500

    Add Cinematic Camera and Post Effects

---
+++ Assets/Game_Asset/Scripts/PostEffects/SimpleFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Game_Asset.Scripts.PostEffects
+{
+    public class SimpleFilter : MonoBehaviour
+    {
+        public Shader effectShader;
+        protected Material Material;
+        [SerializeField] protected bool useFilter = true;
+
+        private void Awake()
+        {
+            Material = new Material(effectShader);
+            Init();
+        }
+
+        protected virtual void Init()
+        {
+
+        }
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dst)
+        {
+            if (useFilter)
+            {
+                UseFilter(src, dst);
+            }
+            else
+            { 
+                Graphics.Blit(src, dst); 
+            }
+        }
+
+        protected virtual void UseFilter(RenderTexture src, RenderTexture dst)
+        {
+            Graphics.Blit(src, dst, Material);
+        }
+    }
+}
---
+++ Assets/Game_Asset/Scripts/PostEffects/VignetteFilter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Game_Asset.Scripts.PostEffects
+{
+    public class VignetteFilter : SimpleFilter
+    {
+        public Vector2 offset;
+        public float exp;
+        public Color color;
+
+        private static readonly int xOffset = Shader.PropertyToID("_xOffset");
+        private static readonly int yOffset = Shader.PropertyToID("_yOffset");
+        private static readonly int Exp = Shader.PropertyToID("_Exp");
+        private static readonly int Color = Shader.PropertyToID("_Color");
+
+
+        protected override void UseFilter(RenderTexture src, RenderTexture dst)
+        {
+            Material.SetFloat(xOffset, offset.x);
+            Material.SetFloat(yOffset, offset.y);
+            Material.SetFloat(Exp, exp);
+            Material.SetColor(Color, color);
+
+            Graphics.Blit(src, dst, Material);
+        }
+    }
+}
