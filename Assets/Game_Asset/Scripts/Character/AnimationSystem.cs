f632002e525cbc20232b55e4227e5127b907d962
Ars-Mond <ars123.www@gmail.com>
2021-11-06 13:09:51 +0500

    Created Character
    Created CharacterAnimation
    Created CharacterIKSystem
    Created CharacterGraphic
    Created CharacterColliderSystem

---
+++ Assets/Game_Asset/Scripts/Character/AnimationSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Game_Asset.Scripts.Character
+{
+    public class AnimationSystem : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private PhysicsMovement2D physicsMovement2D;
+        private static readonly int Speed = Animator.StringToHash("Speed");
+
+        private void Awake()
+        {
+            if (!animator) throw new NullReferenceException($"{nameof(animator)} is null!");
+            if (!physicsMovement2D) throw new NullReferenceException($"{nameof(physicsMovement2D)} is null!");
+        }
+
+        private void Update() => animator.SetFloat(Speed, physicsMovement2D.Velocity);
+    }
+}
