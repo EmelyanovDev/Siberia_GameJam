7f9bf538a04c1cd4817208142423409389538576
Emelyanov <anton575329@gmail.com>
2021-11-06 21:12:54 +0300

    Create Start Scene

--- Assets/Game_Asset/Scripts/Storyline/Apple.cs
+++ Assets/Game_Asset/Scripts/Storyline/Apple.cs
@@ -1,16 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using Game_Asset.Scripts.Character;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
 public class Apple : MonoBehaviour
 {
     [SerializeField] private float _jumpOffDelay;
+    [SerializeField] private float _jumpFromPlayerForce;
+
+    [SerializeField] private KeyboardInput _playerKeyboardInput;
     private Rigidbody2D _rigidbody;
+    private CircleCollider2D _circleCollider;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _circleCollider = GetComponent<CircleCollider2D>();
         
         Invoke(nameof(JumpOff), _jumpOffDelay);
     }
@@ -18,5 +23,18 @@ public class Apple : MonoBehaviour
     private void JumpOff()
     {
         _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Character"))
+        {
+            _rigidbody.AddForce(Vector3.right * _jumpFromPlayerForce);
+            _rigidbody.AddForce(Vector3.up * _jumpFromPlayerForce);
+
+            _circleCollider.enabled = false;
+            _playerKeyboardInput.enabled = true;
+        }
+            
     }
 }
