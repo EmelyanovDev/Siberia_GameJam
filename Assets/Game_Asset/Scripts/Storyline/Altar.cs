7eaca505f00254bb522f04011de405b062069ee1
Emelyanov <anton575329@gmail.com>
2021-11-07 11:36:57 +0300

    Create "Temple" scene

--- Assets/Game_Asset/Scripts/Storyline/Altar.cs
+++ Assets/Game_Asset/Scripts/Storyline/Altar.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 public class Altar : MonoBehaviour
 {
     [SerializeField] private Transform _playerTransform;
-    [SerializeField] private Transform _starTemplate;
+    [SerializeField] private GameObject _starTemplate;
+    [SerializeField] private Transform _starEndPoint;
+    [SerializeField] private float _starSpeedMove;
+
+    private GameObject star;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        var star = Instantiate(_starTemplate, _playerTransform);
+        if(other.gameObject.layer == LayerMask.NameToLayer("TriggerInitiator"))
+            star = Instantiate(_starTemplate, _playerTransform.position, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        if (star != null)
+        {
+            if (star.transform.position != _starEndPoint.position)
+            {
+                star.transform.position = Vector3.MoveTowards(star.transform.position, _starEndPoint.position,
+                    _starSpeedMove * Time.deltaTime);
+            }
+        }
     }
 }
